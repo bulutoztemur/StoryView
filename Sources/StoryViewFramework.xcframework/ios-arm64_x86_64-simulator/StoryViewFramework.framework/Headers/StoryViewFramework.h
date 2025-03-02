9df0f3b661c7ff3154e063a76d3a33b431d9264d
bulut.oztemur <bulutoztemur@gmail.com>
2025-03-02 22:48:37 +0100

    First commit

---
+++ Sources/StoryViewFramework.xcframework/ios-arm64_x86_64-simulator/StoryViewFramework.framework/Headers/StoryViewFramework.h
@@ -0,0 +1,18 @@
+//
+//  StoryViewFramework.h
+//  StoryViewFramework
+//
+//  Created by bulut.oztemur on 02.03.25.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for StoryViewFramework.
+FOUNDATION_EXPORT double StoryViewFrameworkVersionNumber;
+
+//! Project version string for StoryViewFramework.
+FOUNDATION_EXPORT const unsigned char StoryViewFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <StoryViewFramework/PublicHeader.h>
+
+
